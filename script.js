ddb4cfcdb602766a38a458eb304a0172be66abec
Wisdom Alawode <alawodewisdom02@gmail.com>
2026-09-14 18:44:33 +0100

    getting the nodes and making the display responsive to the buttons

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-//connecting the one button node
+//connecting the button nodes
 const one = document.querySelector(".one");
+const two = document.querySelector("two");
 
+//getting the display nodes
+const display = document.querySelector("#display")
+
+//making the display responsive to the buttons
 one.addEventListener("click", function (){
-    console.log("one was clicked");
+    display.value="1";
+});
+
+two.addEventListener("click", function(){
+    display.value = "2";
 })
