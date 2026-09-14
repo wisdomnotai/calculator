8c6a0232abb478ef14e1acaf0e35960acd7c9ae7
Wisdom Alawode <alawodewisdom02@gmail.com>
2026-09-14 19:49:38 +0100

    adding the plus functionality

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+//creating variables for memory
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
 //connecting the button nodes
 const one = document.querySelectorAll(".one");
 const zero = document.querySelectorAll(".zero");
 const clear = document.querySelector(".clear");
 const backspace = document.querySelector(".delete")
+const add = document.querySelector(".add");
+const calculate = document.querySelector(".calculate");
 //getting the display nodes
 const display = document.querySelector("#display")
 
@@ -27,3 +33,10 @@ clear.addEventListener("click", () =>{
 backspace.addEventListener("click", () => {
     display.value= display.value.slice(0,-1);
 });
+//adding the plus functionality
+add.addEventListener("click", () =>{
+    firstNumber = display.value;
+    operator = "+";
+    display.value = "";
+
+})
