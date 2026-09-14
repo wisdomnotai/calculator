8d0ed290891a64d4a7f7f36f476ecf76d89215d0
Wisdom Alawode <alawodewisdom02@gmail.com>
2026-09-14 19:28:04 +0100

    creating the backspace fucntionality

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const one = document.querySelectorAll(".one");
 const zero = document.querySelectorAll(".zero");
 const clear = document.querySelector(".clear");
+const backspace = document.querySelector(".delete")
 //getting the display nodes
 const display = document.querySelector("#display")
 
@@ -21,4 +22,8 @@ zero.forEach((button) => {
 //adding the clear functionality
 clear.addEventListener("click", () =>{
     display.value = "";
+});
+//adding the delete functionality
+backspace.addEventListener("click", () => {
+    display.value= display.value.slice(0,-1);
 });
